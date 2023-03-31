ec8724692feeaaa9fb2baab629f7352882759eb8
MorganaTBorges <morgana.t.borges@gmail.com>
2023-03-31 23:08:25 +0000

    add action_text

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+  validates_presence_of :title, :description
+  validates_length_of :description, minimum: 5
+  has_rich_text :description
+  def to_s
+    title
+  end
 end
